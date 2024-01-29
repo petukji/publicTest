6cca2825e637dc4b1cafe1a96702545edce94f92
One17Technologies <ratzzdevelopers@gmail.com>
2024-01-29 14:25:31 +0530

    new1

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-const express =require('express');
-const app=express();
-app.use(express.json());
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const cors = require('cors');
+app.use(cors({ origin: true }))
 
 
-app.post("/Frenzo", async (req, res) => {
+app.post('/temp', async (req, res) => {
     try {
-        const {
-            country,
-            action
-        } = req.body;
-        //return res.json("Subhash Rana");
-        res.status(201).json({msg:'vendor registered'})   
+        const { var1, var2 } = req.body;
+        const result = var1 + var2;
+        res.json({ result });
     } catch (error) {
         console.error(error);
-        return res.status(500).json({ error: 'Internal Server Error/Invalid Input' });
+         res.status(500).json({ error: 'Internal Server Error/Invalid Input' });
     }
-   
 });
 app.get("/", (req, res) => {
     res.send("Hello World!");
