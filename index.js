034e8bcc4b76fd811d1812c6ba080d0ea64c5c15
One17Technologies <ratzzdevelopers@gmail.com>
2024-01-29 13:30:19 +0530

    t1

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ const app=express();
 app.use(express.json());
 
 
-app.post('/Frenzo', async (req, res) => {
+app.post("/Frenzo", async (req, res) => {
     try {
         const {
             country,
             action
         } = req.body;
-        return res.json("Subhash Rana");
+        //return res.json("Subhash Rana");
+        res.status(201).json({msg:'vendor registered'})   
     } catch (error) {
         console.error(error);
         return res.status(500).json({ error: 'Internal Server Error/Invalid Input' });
     }
+   
 });
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
